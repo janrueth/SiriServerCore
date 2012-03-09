68573439bd13a9e3ffa83dc541d44f3ee3b69c40
Eichhoernchen <EHoernchen@gmail.com>
2012-03-09 14:59:58 +0100

    added certificate generation if no certificate is present

--- SiriServer.py
+++ SiriServer.py
@@ -21,13 +21,16 @@ except ImportError:
         print "Warning: Your system does not support poll"
         print "\t Will use default select interface"
         
-from twisted.internet import reactor
-    
 from SiriProtocolHandler import SiriProtocolHandler
 from optparse import OptionParser
+from os.path import exists
+from socket import gethostname
+from twisted.internet import reactor
 import PluginManager
 import db
 import logging
+import sys
+    
 try:       
     from OpenSSL import crypto
 except:
@@ -79,9 +82,122 @@ class SiriFactory(Factory):
         logging.getLogger().info("Server is shutting down")
         self.dbConnection.close()
         logging.getLogger().info("Database Connection Closed")
+        
+        
 
+
+ROOT_CA_CERT_FILE = "keys/ca.pem"
+ROOT_CA_KEY_FILE = "keys/cakey.pem"
+SERVER_CERT_FILE = "keys/server.crt"
+SERVER_KEY_FILE = "keys/server.key"
+
+def create_self_signed_cert():
+    
+    if not exists(SERVER_CERT_FILE) or not exists(SERVER_KEY_FILE) or not exists(ROOT_CA_CERT_FILE):
+
+        print "I could not find valid certificates. I will now guide you through the process of creating some."
+        
+        print "I will create a Certification Authority (CA) first"
+        # create a key pair
+        ca_key = crypto.PKey()
+        ca_key.generate_key(crypto.TYPE_RSA, 2048)
+
+        # create a self-signed cert
+        CAcert = crypto.X509()
+        CAcert.get_subject().C = "DE"
+        CAcert.get_subject().ST = "NRW"
+        CAcert.get_subject().L = "Aachen"
+        CAcert.get_subject().O = "SiriServer by Eichhoernchen"
+        CAcert.get_subject().OU = "SiriServer Certificate Authority"
+        CAcert.get_subject().CN = "SiriServer Fake CA Certificate"
+        CAcert.set_serial_number(1000)
+        CAcert.gmtime_adj_notBefore(0)
+        CAcert.gmtime_adj_notAfter(10*365*24*60*60)
+        CAcert.set_issuer(CAcert.get_subject())
+        CAcert.set_pubkey(ca_key)
+        
+        extensions = []
+        crypto.X509ExtensionType
+        extensions.append(crypto.X509Extension("basicConstraints", critical=False, value="CA:TRUE"))
+        extensions.append(crypto.X509Extension("subjectKeyIdentifier", critical=False, value="hash", subject=CAcert))
+        CAcert.add_extensions(extensions)
+        # we need to set this separatly... don't know why...
+        CAcert.add_extensions([crypto.X509Extension("authorityKeyIdentifier", critical=False, value="keyid:always", issuer=CAcert)])
+        CAcert.sign(ca_key, 'sha1')
+        fhandle = open(ROOT_CA_CERT_FILE, "wb")
+        fhandle.write(
+            crypto.dump_certificate(crypto.FILETYPE_PEM, CAcert)
+            )
+        fhandle.close()
+        fhandle = open(ROOT_CA_KEY_FILE, "wb")
+        fhandle.write(
+            crypto.dump_privatekey(crypto.FILETYPE_PEM, ca_key)
+            )
+        fhandle.close()
+        
+        print "I successfully created a CA for you, will now use it to create SSL certificates."
+        # create a key pair
+        k2 = crypto.PKey()
+        k2.generate_key(crypto.TYPE_RSA, 2048)
+
+        # create a self-signed cert
+        cert = crypto.X509()
+        cert.get_subject().C = "DE"
+        cert.get_subject().ST = "NRW"
+        cert.get_subject().L = "Aachen"
+        cert.get_subject().O = "SiriServer by Eichhoernchen"
+        cert.get_subject().OU = "SiriServer Certificate Authority"
+        
+        hostname = gethostname()
+        print "We need to set the correct address of this machine in the certificate."
+        print "I will now query your system for its name. But it might be that you want to use a DNS name I cannot find.\n"
+        print "-------- IMPORTANT ----------"
+        print "Your system tells me that your hostname/IP is: {0}".format(hostname)
+        sys.stdout.write("Do you want to use this information (y/n): ")
+        answer = sys.stdin.readline().lower()
+        if "no" in answer or "n" in answer:
+            sys.stdout.write("Okay what do you want the hostname to be: ")
+            hostname = sys.stdin.readline().strip()
+            print "Okay thanks I will now be using {0}".format(hostname)
+            
+        cert.get_subject().CN = hostname
+        cert.set_serial_number(1000)
+        cert.gmtime_adj_notBefore(0)
+        cert.gmtime_adj_notAfter(10*365*24*60*60)
+        cert.set_issuer(CAcert.get_subject())
+        cert.set_pubkey(k2)
+        
+        extensions = []
+        crypto.X509ExtensionType
+        extensions.append(crypto.X509Extension("basicConstraints", critical=False, value="CA:FALSE"))
+        extensions.append(crypto.X509Extension("subjectKeyIdentifier", critical=False, value="hash", subject=cert))
+        extensions.append(crypto.X509Extension("authorityKeyIdentifier", critical=False, value="keyid:always", subject=CAcert, issuer=CAcert))
+        cert.add_extensions(extensions)
+        
+        cert.sign(ca_key, 'sha1')
+        fhandle = open(SERVER_CERT_FILE, "wb")
+        fhandle.write(
+            crypto.dump_certificate(crypto.FILETYPE_PEM, cert)
+            )
+        fhandle.close()
+        fhandle = open(SERVER_KEY_FILE, "wb")
+        fhandle.write(
+            crypto.dump_privatekey(crypto.FILETYPE_PEM, k2)
+            )
+        fhandle.close()
+        
+        print """
+        \t\t\t ------------ IMPORTANT ------------\n\n
+        \t\tAll certificates have been generated... You must now install the CA certificate on your device\n\n
+        \t\tThe file is located at keys/ca.pem in your SiriServer root\n\n
+        \t\tMake sure to uninstall an old CA certificate first"\n\n
+        \t\tTHE CERTIFICATES MUST MATCH! IF YOU DID THIS HERE BEFORE, THE OLD ca.pem WON'T WORK ANYMORE\n
+        \t\tYou can just EMail the keys/ca.pem file to yourself\n
+        """
+    
+        
 def main():
-
+    
     parser = OptionParser()
     parser.add_option('-l', '--loglevel', default='info', dest='logLevel', help='This sets the logging level you have these options: debug, info, warning, error, critical \t\tThe standard value is info')
     parser.add_option('-p', '--port', default=4443, type='int', dest='port', help='This options lets you use a custom port instead of 443 (use a port > 1024 to run as non root user)')
@@ -100,9 +216,10 @@ def main():
     h.setFormatter(f)
     x.addHandler(h)
     
+    create_self_signed_cert()
     
     x.info("Starting server on port {0}".format(options.port))
-    reactor.listenSSL(options.port, SiriFactory(), ssl.DefaultOpenSSLContextFactory('keys/server.key', 'keys/server.crt'))
+    reactor.listenSSL(options.port, SiriFactory(), ssl.DefaultOpenSSLContextFactory(SERVER_KEY_FILE, SERVER_CERT_FILE))
     reactor.run()
     x.info("Server shutdown complete")
     
