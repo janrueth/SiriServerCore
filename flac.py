c09362a5c7a0146b21a13cd6cd45f122ddd6d3c9
Eichhoernchen <EHoernchen@gmail.com>
2012-03-21 10:24:10 +0100

    little speedup for flac

--- flac.py
+++ flac.py
@@ -12,10 +12,10 @@ if libflac_name == None:
 libflac = CDLL(libflac_name)
 
 
-def writeCallBack(encoder, buffer, bytes, samples, current_frame, client_data):
+def writeCallBack(encoder, buf, numBytes, samples, current_frame, client_data):
     print "Test"
     instance = cast(client_data, py_object).value
-    return Encoder.internalCallBack(instance, encoder, buffer, bytes, samples, current_frame)
+    return Encoder.internalCallBack(instance, encoder, buf, numBytes, samples, current_frame)
 
 
 
@@ -74,16 +74,16 @@ class Encoder:
         
         self.output = ""
         #libflac.FLAC__stream_encoder_init_stream(self.encoder, writeCallBackFUN(writeCallBack), None, None, None, py_object(self))
-        file = tempfile.NamedTemporaryFile(delete=False)
-        file.close()
-        self.filename = file.name
+        tmpFile = tempfile.NamedTemporaryFile(delete=False)
+        tmpFile.close()
+        self.filename = tmpFile.name
         libflac.FLAC__stream_encoder_init_file(self.encoder, self.filename, None, None)
         if not ok:
             print "Error initializing libflac"
             exit()
     
-    def internalCallBack(self, encoder, buffer, bytes, samples, current_frame):
-        self.output += string_at(buffer, bytes)
+    def internalCallBack(self, encoder, buf, numBytes, samples, current_frame):
+        self.output += string_at(buf, numBytes)
         print self.output
         return 0
     
@@ -92,7 +92,7 @@ class Encoder:
     def encode(self, data):
         length = int(len(data)/2)
         int16s = struct.unpack('<' + ('h'*length), data)
-        int32s = (c_int32*len(int16s))(*int16s)
+        int32s = struct.pack('@' + ('i'*length), *int16s)
         libflac.FLAC__stream_encoder_process_interleaved(self.encoder, int32s, length)
 
     def getBinary(self):
